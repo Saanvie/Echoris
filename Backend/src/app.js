71ec322c49df0f542737f526102960ea69a080ad
Saanvie <saanviedadwal2425@gmail.com>
2025-04-04 22:12:55 +0530

    Commiting backend without sockets

---
+++ Backend/src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import http from "node:http";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js"
+
+import cors from "cors";
+import userRoutes from "./routes/users.route.js";
+
+const app = express();
+const server = http.createServer(app); // Creting http server and connecting it with express, i.e my system
+const io = connectToSocket(server); // Attaching a Socket.io server to the HTTP server ,, server is a parameter which is passed here
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb" , extended: "true"}));
+
+app.use("/api/v1/users", userRoutes);
+
+
+
+const start = async() => {  
+    const connectionDb = await mongoose.connect("mongodb+srv://SaanvieDadwal:Mongodb001@echoris-db.a6wt3fc.mongodb.net/")
+    
+    console.log(`Mongo connected db Host: ${connectionDb.connection.host}`);
+    server.listen(app.get("port"), () => {
+        console.log(`Listening on port ${app.get("port")}`)
+    })
+}
+
+start();
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import {Schema} from "mongoose";
+
+const userSchema = new Schema(
+    {
+        name: {type: String, required: true},
+        username: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+        token: {type: String}
+    }
+)
+
+const User = mongoose.model("User", userSchema); // jaise hum koi object create krte hai uske saamne new likhkr and then fior uss blueprint ke according apni object bnate hai vhi same kaam yha par .model krta hai, i.e. hum pehle usse user dere hai 
+
+export { User };
